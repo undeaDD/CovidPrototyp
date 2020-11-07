124b5e94c577cf0f1ea5b1607d47b3d12d1ad7e6
Dominic Drees <undeaD_D@live.de>
2020-11-07 22:03:18 +0100

    swap bootstrap-tables for newer jquery datatabes

--- assets/js/patients.js
+++ assets/js/patients.js
@@ -8,11 +8,34 @@ firebase.initializeApp({
     appId: "1:857488715212:web:763e73804d890e4bf40169"
 });
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (!user) {
-        firebase.auth().signOut();
-        window.location.href = "einsendeschein.html";
-    } else {
-        document.body.classList.remove("d-none");
-    }
-});
+function snapshotToArray(snapshot) {
+    var returnArr = [];
+
+    snapshot.forEach(function(childSnapshot) {
+        var item = childSnapshot.val();
+        item.key = childSnapshot.key;
+
+        returnArr.push(item);
+    });
+
+    return returnArr;
+};
+
+$(function() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (!user) {
+            firebase.auth().signOut();
+            window.location.href = "einsendeschein.html";
+        } else {
+            document.body.classList.remove("d-none");
+
+            firebase.database().ref('requests').on('value', function(snapshot) {
+                $('#table1').DataTable();
+            });
+
+            firebase.database().ref('patients').on('value', function(snapshot) {
+                $('#table2').DataTable();
+            });
+        }
+    });
+})
