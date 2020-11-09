10b9773cdc250f2744493ee02089666a7e77009d
Dominic Drees <undeaD_D@live.de>
2020-11-09 16:24:15 +0100

    load data from database, tablestyling, use datatables instead of bootstrap-tables, ...

--- assets/js/patients.js
+++ assets/js/patients.js
@@ -13,8 +13,7 @@ function snapshotToArray(snapshot) {
 
     snapshot.forEach(function(childSnapshot) {
         var item = childSnapshot.val();
-        item.key = childSnapshot.key;
-
+        item.id = childSnapshot.key;
         returnArr.push(item);
     });
 
@@ -30,12 +29,172 @@ $(function() {
             document.body.classList.remove("d-none");
 
             firebase.database().ref('requests').on('value', function(snapshot) {
-                $('#table1').DataTable();
+                var table1 = $('#table1').DataTable({
+                    "destroy": true,
+                    "paging": false,
+                    "ordering": false,
+                    "info": false,
+                    "searching": true,
+                    "fixedHeader": true,
+                    "scrollX": true,
+                    "autoWidth": false,
+                    "language": {
+                        "search": "",
+                        "searchPlaceholder": "Nach Eintrag suchen",
+                        "emptyTable": "Keine Einträge vorhanden",
+                    },
+                    "columns": [
+                        { "data": "id", "className": "bold" },
+                        { "data": "lname" },
+                        { "data": "fname" },
+                        { "data": "street" },
+                        { "data": "plz" },
+                        { "data": "city" },
+                        { "data": "birthday" },
+                        {
+                            "data": "tel",
+                            "defaultContent": "-",
+                            "render": function(data, type, row, meta) {
+                                if (type === 'display' && data != "-") {
+                                    data = '<a href="tel:' + data + '">' + data + '</a>';
+                                }
+                                return data;
+                            }
+                        },
+                        {
+                            "data": "mobile",
+                            "defaultContent": "-",
+                            "render": function(data, type, row, meta) {
+                                if (type === 'display' && data != "-") {
+                                    data = '<a href="tel:' + data + '">' + data + '</a>';
+                                }
+                                return data;
+                            }
+                        },
+                        {
+                            "data": "fax",
+                            "defaultContent": "-"
+                        },
+                        {
+                            "data": "email",
+                            "defaultContent": "-",
+                            "render": function(data, type, row, meta) {
+                                if (type === 'display' && data != "-") {
+                                    data = '<a href="mailto:' + data + '">' + data + '</a>';
+                                }
+                                return data;
+                            }
+                        },
+                        {
+                            "data": "emailCopy",
+                            "defaultContent": "-",
+                            "render": function(data, type, row, meta) {
+                                if (type === 'display' && data != "-") {
+                                    data = '<a href="mailto:' + data + '">' + data + '</a>';
+                                }
+                                return data;
+                            }
+                        },
+                        {
+                            "data": "date1",
+                            "defaultContent": "-"
+                        },
+                        {
+                            "data": "date2",
+                            "defaultContent": "-"
+                        },
+                        {
+                            "data": "date3",
+                            "defaultContent": "-"
+                        },
+                    ],
+                    //"data": snapshotToArray(snapshot),
+                });
             });
 
             firebase.database().ref('patients').on('value', function(snapshot) {
-                $('#table2').DataTable();
+                var table2 = $('#table2').DataTable({
+                    "destroy": true,
+                    "paging": false,
+                    "ordering": false,
+                    "info": false,
+                    "searching": true,
+                    "fixedHeader": true,
+                    "scrollX": true,
+                    "autoWidth": false,
+                    "language": {
+                        "search": "",
+                        "searchPlaceholder": "Nach Eintrag suchen",
+                        "emptyTable": "Keine Einträge vorhanden",
+                    },
+                    "columns": [
+                        { "data": "id", "className": "bold" },
+                        { "data": "lname" },
+                        { "data": "fname" },
+                        { "data": "street" },
+                        { "data": "plz" },
+                        { "data": "city" },
+                        { "data": "birthday" },
+                        {
+                            "data": "tel",
+                            "defaultContent": "-",
+                            "render": function(data, type, row, meta) {
+                                if (type === 'display' && data != "-") {
+                                    data = '<a href="tel:' + data + '">' + data + '</a>';
+                                }
+                                return data;
+                            }
+                        },
+                        {
+                            "data": "mobile",
+                            "defaultContent": "-",
+                            "render": function(data, type, row, meta) {
+                                if (type === 'display' && data != "-") {
+                                    data = '<a href="tel:' + data + '">' + data + '</a>';
+                                }
+                                return data;
+                            }
+                        },
+                        {
+                            "data": "fax",
+                            "defaultContent": "-"
+                        },
+                        {
+                            "data": "email",
+                            "defaultContent": "-",
+                            "render": function(data, type, row, meta) {
+                                if (type === 'display' && data != "-") {
+                                    data = '<a href="mailto:' + data + '">' + data + '</a>';
+                                }
+                                return data;
+                            }
+                        },
+                        {
+                            "data": "emailCopy",
+                            "defaultContent": "-",
+                            "render": function(data, type, row, meta) {
+                                if (type === 'display' && data != "-") {
+                                    data = '<a href="mailto:' + data + '">' + data + '</a>';
+                                }
+                                return data;
+                            }
+                        },
+                        {
+                            "data": "date1",
+                            "defaultContent": "-"
+                        },
+                        {
+                            "data": "date2",
+                            "defaultContent": "-"
+                        },
+                        {
+                            "data": "date3",
+                            "defaultContent": "-"
+                        },
+                    ],
+                    "data": snapshotToArray(snapshot),
+                });
             });
         }
     });
-})
+});
